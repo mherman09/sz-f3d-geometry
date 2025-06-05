21e02734979f4965bd7768600f44ab53ddb9593d
Matt Herman <mherman2@csub.edu>
2025-06-05 14:30:03 -0700

    Trim trench to model domain

--- generate_points.sh
+++ generate_points.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-echo "----------------------------------------------------------------------------------------------------"
-echo "$0: starting"
-echo "----------------------------------------------------------------------------------------------------"
-
 
 #####
 #   USER-DEFINED MODEL PARAMETERS
 #####
 
-# Geographic corners of the model (do they need to be ordered in a certain direction?)
+# Geographic corners of the model (ordered)
 LON_CORNER_1=-75.0
 LAT_CORNER_1=-35.0
 LON_CORNER_2=-75.0
@@ -26,7 +22,7 @@ D="30"
 THICKNESS=50
 
 # Trench file
-TRENCH_FILE=/Users/mherman2/Dropbox/south_america_illapel_cycle/model_geometry/sam_trench_dep.txt
+TRENCH_FILE=/home/mherman2/Research/slab2/Slab2Distribute_Mar2018/trenches/sam_slab2_trench_02.23.18.xyz
 
 # Trench depth (km)
 TRENCH_DEPTH=-6  # User-defined trench depth
@@ -37,13 +33,60 @@ TRENCH_DEPTH=AUTO  # Mean trench depth from input file
 
 
 
+
+
+####################################################################################################
+####################################################################################################
 ####################################################################################################
 #------------------------------------ MODIFY AT YOUR OWN RISK! ------------------------------------#
 #-------------------------------- EMAIL mherman2@csub.edu FOR HELP --------------------------------#
 ####################################################################################################
+####################################################################################################
+####################################################################################################
 
 
+
+
+#####
+#   INITIALIZE
+#####
 EPOCH=`date "+%s"`
+SCRIPT=`basename $0`
+function timenow () {
+    date "+%H:%M:%S"
+}
+
+# Clean up function
+function cleanup () {
+    rm -f intersect_side_${SCRIPT}_${EPOCH}.tmp
+    rm -f intersect_${SCRIPT}_${EPOCH}.tmp
+    rm -f intersect_[1-4]_${SCRIPT}_${EPOCH}.tmp
+    rm -f line_${SCRIPT}_${EPOCH}.tmp
+    rm -f trench_${SCRIPT}_${EPOCH}.tmp
+}
+trap "cleanup" 0 1 2 3 8 9
+
+
+echo "$SCRIPT [`timenow`]: starting"
+
+
+
+
+
+
+#####
+#   CHECKS
+#####
+
+echo "$SCRIPT [`timenow`]: checking inputs"
+
+# Look for input trench file
+if [ -z $TRENCH_FILE ]; then echo "$SCRIPT [ERROR]: no trench file defined" 1>&2 ; exit 1; fi
+test -f $TRENCH_FILE || { echo "$SCRIPT [ERROR]: could not find trench file $TRENCH_FILE" 1>&2 ; exit 1 ; }
+
+echo
+
+
 
 
 
@@ -51,9 +94,11 @@ EPOCH=`date "+%s"`
 #   CUT TRENCH TO MODEL BOUNDARIES
 #####
 
+echo "$SCRIPT [`timenow`]: trimming trench"
+
 
 # Make all longitudes in range 0-360
-echo $0: setting longitudes to be in range 0-360
+echo "$SCRIPT [`timenow`]: setting corner longitudes to be in range 0-360"
 LON_CORNER_1=`echo $LON_CORNER_1 | awk '{if($1<0){print $1+360}else{print $1}}'`
 LON_CORNER_2=`echo $LON_CORNER_2 | awk '{if($1<0){print $1+360}else{print $1}}'`
 LON_CORNER_3=`echo $LON_CORNER_3 | awk '{if($1<0){print $1+360}else{print $1}}'`
@@ -62,9 +107,11 @@ LON_CORNER_4=`echo $LON_CORNER_4 | awk '{if($1<0){print $1+360}else{print $1}}'`
 
 
 # For each side of the model, test whether trench intersects it
-echo $0: determining where trench intersects model sides
+echo "$SCRIPT [`timenow`]: determining where trench intersects model sides"
+
 for ISIDE in 1 2 3 4
 do
+
     # Get corner coordinates for the side
     case $ISIDE in
         1) LO1=$LON_CORNER_1; LA1=$LAT_CORNER_1; LO2=$LAT_CORNER_2; LA2=$LAT_CORNER_2;;
@@ -72,48 +119,50 @@ do
         3) LO1=$LON_CORNER_3; LA1=$LAT_CORNER_3; LO2=$LAT_CORNER_4; LA2=$LAT_CORNER_4;;
         4) LO1=$LON_CORNER_4; LA1=$LAT_CORNER_4; LO2=$LAT_CORNER_1; LA2=$LAT_CORNER_1;;
     esac
+
     # Calculate distance and azimuth along side
     DIST=`lola2distaz -c $LO1 $LA1 $LO2 $LA2 | awk '{print $1}'`
     AZ=`lola2distaz -c $LO1 $LA1 $LO2 $LA2 | awk '{print $2}'`
+
     # Divide side into 1000 segments
     grid -x 0 $DIST -nx 1000 |\
         awk '{print '$LO1','$LA1',$1,'$AZ'}' |\
         distaz2lola -f stdin |\
-        awk 'BEGIN{print ">"}{print $0}END{print ">"}' > intersect_side.tmp
+        awk 'BEGIN{print ">"}{print $0}END{print ">"}' > intersect_side_${SCRIPT}_${EPOCH}.tmp
+
     # Put side segments and trench segments into file for gmtspatial intersection calculation
-    cat intersect_side.tmp $TRENCH_FILE > intersect.tmp
-    INTERSECTION=`gmt gmtspatial intersect.tmp -Ie | sed -ne "1p" | awk '{print $1,$2}'`
+    cat intersect_side_${SCRIPT}_${EPOCH}.tmp $TRENCH_FILE > intersect_${SCRIPT}_${EPOCH}.tmp
+    INTERSECTION=`gmt gmtspatial intersect_${SCRIPT}_${EPOCH}.tmp -Ie | sed -ne "1p" | awk '{print $1,$2}'`
     if [ "$INTERSECTION" != "" ]
     then
-        echo $INTERSECTION > intersect_$ISIDE.tmp
+        echo $INTERSECTION > intersect_${ISIDE}_${SCRIPT}_${EPOCH}.tmp
     fi
 done
-# Clean up
-rm intersect_side.tmp intersect.tmp
 
 
 
 # Check that there are 2 and only 2 intersections between trench and model sides
-echo $0: checking that there are only 2 intersections
-NINTERSECTIONS=`ls intersect_[1-4].tmp | wc | awk '{print $1}'`
+echo "$SCRIPT [`timenow`]: checking that there are only 2 intersections"
+NINTERSECTIONS=`ls intersect_[1-4]_${SCRIPT}_${EPOCH}.tmp 2> /dev/null | wc | awk '{print $1}'`
 if [ "$NINTERSECTIONS" != "2" ]
 then
-    echo "$0 [ERROR]: found $NINTERSECTIONS intersections between trench and model edges" 1>&2
-    echo "$0 [ERROR]: there should be 2 ... exiting" 1>&2
+    echo "$SCRIPT [ERROR]: found $NINTERSECTIONS intersections between trench and model edges" 1>&2
+    echo "$SCRIPT [ERROR]: there should be 2 ... exiting" 1>&2
     exit 1
 fi
 
 
-
 # Trim trench to model domain
-echo $0: trimming trench to model domain
+echo "$SCRIPT [`timenow`]: trimming trench to model domain"
+
 # Remove temporary line file if present
-test -f line.tmp && rm -f line.tmp
+test -f line_${SCRIPT}_${EPOCH}.tmp && rm -f line_${SCRIPT}_${EPOCH}.tmp
+
 # Get indices of points on trench between each intersection
 for I in 1 2
 do
-    LO0=`cat intersect_[1-4].tmp | sed -ne "${I}p" | awk '{print $1}'`
-    LA0=`cat intersect_[1-4].tmp | sed -ne "${I}p" | awk '{print $2}'`
+    LO0=`cat intersect_[1-4]_${SCRIPT}_${EPOCH}.tmp | sed -ne "${I}p" | awk '{print $1}'`
+    LA0=`cat intersect_[1-4]_${SCRIPT}_${EPOCH}.tmp | sed -ne "${I}p" | awk '{print $2}'`
     awk 'BEGIN{lo0='$LO0';la0='$LA0'}{
         if (NR>2) {
             lo2 = $1
@@ -130,33 +179,40 @@ do
         }
         lo1 = $1
         la1 = $2
-    }' $TRENCH_FILE >> line.tmp
+    }' $TRENCH_FILE >> line_${SCRIPT}_${EPOCH}.tmp
 done
-NR_START=`sort -g line.tmp | awk '{if(NR==1){print $2}}'`
-NR_END=`sort -g line.tmp | awk '{if(NR==2){print $1}}'`
-PT_START=`sort -g line.tmp | awk '{if(NR==1){print $3,$4}}'`
-PT_END=`sort -g line.tmp | awk '{if(NR==2){print $3,$4}}'`
-# Trim trench file
+NR_START=`sort -g line_${SCRIPT}_${EPOCH}.tmp | awk '{if(NR==1){print $2}}'`
+NR_END=`sort -g line_${SCRIPT}_${EPOCH}.tmp | awk '{if(NR==2){print $1}}'`
+
+# Save starting and ending points
+PT_START=`sort -g line_${SCRIPT}_${EPOCH}.tmp | awk '{if(NR==1){print $3,$4}}'`
+PT_END=`sort -g line_${SCRIPT}_${EPOCH}.tmp | awk '{if(NR==2){print $3,$4}}'`
+
+# Trim trench file, saving points at sampling interval
 awk 'BEGIN{print "'"$PT_START"'"}{
     if ('$NR_START'<=NR && NR<='$NR_END') {print $0}
-}END{ print "'"$PT_END"'"}' $TRENCH_FILE > trench_${EPOCH}.tmp
-# Clean up
-rm intersect_[1-4].tmp line.tmp
+}END{ print "'"$PT_END"'"}' $TRENCH_FILE > trench_${SCRIPT}_${EPOCH}.tmp
+
+
+
+# Sample to user-specified distance scale
+echo "$SCRIPT [`timenow`]: downsampling trench to points every $D km"
+downsample_line.sh -f trench_${SCRIPT}_${EPOCH}.tmp -d $D -o trench_${SCRIPT}_${EPOCH}.tmp || exit 1
 
 
 
 # Get trench depth
-echo $0: setting trench depth
+echo "$SCRIPT [`timenow`]: setting trench depth"
+
+# Calculate or manually set trench depth
 if [ "$TRENCH_DEPTH" == "AUTO" ]
 then
-    echo $0: AUTO mode - calculating mean trench depth
-    TRENCH_DEPTH=`awk 'BEGIN{sum=0;n=0}{if(NF==3){sum+=$3;n++}}END{print sum/n}' trench_${EPOCH}.tmp`
-    echo $0: mean trench depth is $TRENCH_DEPTH km
+    echo "$SCRIPT [`timenow`]: AUTO mode - calculating mean trench depth"
+    TRENCH_DEPTH=`awk 'BEGIN{sum=0;n=0}{if(NF==3){sum+=$3;n++}}END{printf("%.1f"),sum/n}' trench_${SCRIPT}_${EPOCH}.tmp`
+    echo "$SCRIPT [`timenow`]: mean trench depth is $TRENCH_DEPTH km"
 else
-    echo $0: USER mode - setting mean trench depth to $TRENCH_DEPTH km
+    echo "$SCRIPT [`timenow`]: USER mode - setting mean trench depth to $TRENCH_DEPTH km"
 fi
-rm trench_${EPOCH}.tmp
-
 
 
 
